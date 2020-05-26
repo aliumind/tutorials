3157d37af758b3233c738616246f8240da3779af
Valerio Mellini <valerio.mellini87@icloud.com>
2020-05-26 21:02:21 +0200

    modifiche

---
+++ 1-python/1-basics/01-variables.py
@@ -0,0 +1,3 @@
+v_integer = 1
+v_float = 1.1
+v_string = "Hello World"
--- 1-python/1-basics/04-functions.py
+++ 1-python/1-basics/04-functions.py
@@ -1,9 +1,14 @@
 def sum(a, b, c=0):
     return a+b+c
 
-print(sum(1,2))
-print(sum(1,2,3))
-print(sum(a=1, b=2,c=3))
 
-arg1, arg2, arg3 = 1,2,3
+def mul(a, b):
+    return a*b
+
+
+print(sum(1, 2))
+print(sum(1, 2, 3))
+print(sum(a=1, b=2, c=3))
+
+arg1, arg2, arg3 = 1, 2, 3
 print(sum(arg1, b=arg2, c=arg3))
